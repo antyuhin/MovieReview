a5f6495caef67e433e5edae0cbaf49b6335307b0
andrew_ant <andrewant@mail.ru>
2021-09-28 22:49:18 +0300

    Initial Commit

---
+++ MovieReviewSwiftUI/MovieReviewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieReviewSwiftUIApp.swift
+//  MovieReviewSwiftUI
+//
+//  Created by Andrew Ant on 28.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct MovieReviewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
